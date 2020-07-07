510f736d0e3a8c4b6482ba3020314b4e110f80de
a-qq <49522082+a-qq@users.noreply.github.com>
2020-07-07 06:02:05 +0200

    Add files via upload

---
+++ src/components/Recipe.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+class Recipe extends React.Component {
+    state = {
+        activeRecipe: []
+    }
+    componentDidMount = async () => {
+        const rid = this.props.location.state.rid;
+        const title = this.props.location.state.recipe;
+        const req = await fetch(`https://cors-anywhere.herokuapp.com/https://recipesapi.herokuapp.com/api/search?q=${title}`);
+
+        const res = await req.json();
+        this.setState({ activeRecipe: res.recipes[0] })
+        console.log(this.state.activeRecipe)
+    }
+    render() {
+        const recipe = this.state.activeRecipe;
+        return (
+            <div className="container">
+                {this.state.activeRecipe.length !== 0 &&
+                    <div className="active-recipe">
+                        <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title} />
+                        <h3 className="active-recipe__title">{recipe.title}</h3>
+                        <h4 className="active-recipe__publisher">
+                            Publisher: <span> {recipe.publisher} </span>
+                        </h4>
+                        <p className="active-recipe__website">
+                            Website: <span><a href={recipe.publisher_url}></a>{recipe.publisher_url}</span>
+                        </p>
+                        <button className="active-recipe__button">
+                            <Link to="/">Go Home</Link>
+                        </button>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+export default Recipe;
